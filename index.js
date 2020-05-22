d6744e6f9f17f017264205d174052dff1b718cf1
ravi <punravi73@gmail.com>
2020-05-23 01:11:51 +1000

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import express from 'express';
 // import graphqlHTTP from 'express-graphql';
 // import buildSchema from 'graphql';
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
