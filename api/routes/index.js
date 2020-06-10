06eea57c864a9a48397831261dcdcea489e27671
ravi <punravi73@gamil.com>
2020-06-10 11:26:01 +1000

    session modification on routes login, register & welcom

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,9 +4,14 @@ const middlewear = require('../auth/middlewear');
 const auth = require('../auth/auth');
 
 
-router.get('/', (req, res) => res.render('welcome', {
-    title: 'Welcome'
-}));
+router.get('/', (req, res) => {
+    if (req.session.user) {
+        return res.redirect('./dashboard');
+    };
+    return res.render('welcome', {
+        title: 'Welcome'
+    });
+});
 
 router.get('/dashboard', middlewear, auth.loginAuth, (req, res) => { 
     res.render('dashboard', {
--- api/routes/login.js
+++ api/routes/login.js
@@ -4,16 +4,35 @@ const loginController = require('../controller/loginController');
 
 router.post('/register', loginController.createUser);
 router.post('/login', loginController.login);
-router.get('/register', (req, res) => res.render('register', {
-    title: 'Register'
-}));
-router.get('/login', (req, res) => res.render('login', {
-    title: 'Login'
-}));
+// router.get('/register', (req, res) => res.render('register', {
+//     title: 'Register'
+// }));
+router.get('/register', (req, res) => {
+    if (req.session.user) {
+        return res.redirect('./dashboard');
+    };
+    return res.render('register', {
+        title: 'Register'
+    });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.user) {
+        return res.redirect('./dashboard');
+    };
+    return res.render('login', {
+        title: 'Login'
+    });
+});
+
+// router.get('/login', (req, res) => res.render('login', {
+//     title: 'Login'
+// }));
 router.get('/logout', (req, res) => {
     // reseting the session 
     req.session.user = null;
     res.redirect('./login');
 });
+
 // router.get('/', loginController.deleteUser);
 module.exports = router;
